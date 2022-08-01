a095186fcfaa8ec6704f1107571499e073e26ce4
Francisco Josue <franc1sc0.sv.xd@gmail.com>
2022-07-31 20:04:30 -0600

    404 page

---
+++ php/404.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/404.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <section class="notFound">
+        <div class="img">
+            <img src="https://assets.codepen.io/5647096/backToTheHomepage.png" alt="Back to the Homepage" />
+            <img src="https://assets.codepen.io/5647096/Delorean.png" alt="El Delorean, El Doc y Marti McFly" />
+        </div>
+        <div class="text">
+            <h1>404</h1>
+            <h2>PAGE NOT FOUND</h2>
+            <h3>BACK TO HOME?</h3>
+            <a href="./index.php" class="yes">YES</a>
+            <a href="https://www.youtube.com/watch?v=G3AfIvJBcGo">NO</a>
+        </div>
+    </section>
+</body>
+
+</html>
+<?php
+//xD
+
+?>
