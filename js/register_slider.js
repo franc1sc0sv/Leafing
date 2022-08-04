6842e25e64fb9a8d808e65ce3ebd11f646e5c48a
Francisco Josue <franc1sc0.sv.xd@gmail.com>
2022-08-03 20:10:47 -0600

    login workin 1.0

--- js/register_slider.js
+++
@@ -1,6 +0,0 @@
-const $nexto = document.querySelector(".nexto");
-const $code = document.querySelector(".containerinfo1");
-
-$nexto.addEventListener('click', function () {
-    document.querySelector(".containerinfo1").classList.toggle("showcode")
-}); 
