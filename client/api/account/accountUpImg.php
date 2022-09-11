a1a6a3511513f86a6426a45771338be21c8d0734
franc1sc0sv <105018289+franc1sc0sv@users.noreply.github.com>
2022-09-10 21:15:44 -0600

    IMAGE_VALIDATION

--- client/api/account/accountUpImg.php
+++ client/api/account/accountUpImg.php
@@ -17,11 +17,14 @@ if ($_POST) {
         $filename = $_FILES['image']['name'];
         $ext = pathinfo($filename, PATHINFO_EXTENSION);
 
-        move_uploaded_file($tempImage, "../../public/assets/user_images/profile_images/" . $image);
-        $objConnection = new conection();
-        $sql = "UPDATE `user_data` SET `img_path`='$image' WHERE id_user_data = $id";
-        $objConnection->ejecutar($sql);
-
+        if (!in_array($ext, $allowed)) {
+            header('location:../../public/php/account/cuenta.php');
+        } else {
+            move_uploaded_file($tempImage, "../../public/assets/user_images/profile_images/" . $image);
+            $objConnection = new conection();
+            $sql = "UPDATE `user_data` SET `img_path`='$image' WHERE id_user_data = $id";
+            $objConnection->ejecutar($sql);
+        }
         header('location:../../public/php/account/cuenta.php');
     } else {
         header('location:../../public/php/account/cuenta.php');
